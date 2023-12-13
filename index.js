784234a466b3b0e857562254203fb7d847feb3c6
Your Name <nasos.stamog@gmail.com>
2023-12-13 13:41:54 +0200

    first issue

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // Serve static HTML files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/static', express.static('static'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,30 @@
-import Handlebars from "handlebars";
+if (window.location.pathname === '/') {
+  fetch('https://wiki-ads.onrender.com/categories')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data)
+      const objWithIdOne = data.find(item => item.id === 1);
+      const imgUrlOfIdOne = objWithIdOne ? objWithIdOne.img_url : '';
 
-fetch('https://wiki-ads.onrender.com/categories')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json(); // Change this to response.text() if expecting plain text
-  })
-  .then(data => {
-    // Work with the retrieved data here
-    console.log(data); // For example, log the data to the console
-  })
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-  });
+      // Fetch the template content
+      let template = document.getElementById('test').innerHTML;
+
+      // Compile the template
+      let compiledTemplate = Handlebars.compile(template);
+
+      // Execute the compiled template and store the content in a variable
+      let content = compiledTemplate({ test: imgUrlOfIdOne });
+
+      // Get the placeholder element and set its inner HTML with the compiled content
+      let placeHolder = document.getElementById('test');
+      placeHolder.innerHTML = content;
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
+}
