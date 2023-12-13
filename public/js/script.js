18291d82d43f44f4c8c14b712539d49c3a52f390
Your Name <nasos.stamog@gmail.com>
2023-12-13 13:43:05 +0200

    fix v1

--- public/js/script.js
+++ public/js/script.js
@@ -8,21 +8,9 @@ if (window.location.pathname === '/') {
     })
     .then(data => {
       console.log(data)
-      const objWithIdOne = data.find(item => item.id === 1);
-      const imgUrlOfIdOne = objWithIdOne ? objWithIdOne.img_url : '';
 
-      // Fetch the template content
-      let template = document.getElementById('test').innerHTML;
 
-      // Compile the template
-      let compiledTemplate = Handlebars.compile(template);
-
-      // Execute the compiled template and store the content in a variable
-      let content = compiledTemplate({ test: imgUrlOfIdOne });
-
-      // Get the placeholder element and set its inner HTML with the compiled content
-      let placeHolder = document.getElementById('test');
-      placeHolder.innerHTML = content;
+      
     })
     .catch(error => {
       console.error('There was a problem with the fetch operation:', error);
